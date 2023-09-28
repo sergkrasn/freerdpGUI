b0f0168c4b205dcd78dd075243a7d0bbd92d1a04
Sergey <65020469+sergkrasn@users.noreply.github.com>
2023-09-28 17:19:16 +0500

    Creat

---
+++ cli.py
@@ -0,0 +1,13 @@
+from src.window import main
+import argparse
+from os import path
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='freerdpgui', usage='%(prog)s [--c]')
+    parser.add_argument('config', nargs='*', type=str, default="",
+                        help='Input file name with configuration',)
+    args = parser.parse_args()
+    try:
+        main(args.config[0])
+    except IndexError:
+        main()
