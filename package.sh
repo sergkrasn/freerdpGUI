50ec49ec658170a9158bbf1710c4cbbdb1b9663a
Sergey <65020469+sergkrasn@users.noreply.github.com>
2023-09-28 17:19:45 +0500

    Creat

---
+++ package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Create folders.
+[ -e package ] && rm -r package
+mkdir -p package/opt
+mkdir -p package/usr/share/applications
+mkdir -p package/usr/share/icons/hicolor/scalable/apps
+mkdir -p package/usr/share/mime/packages
+
+# Copy files (change icon names, add lines for non-scaled icons)
+cp -r dist/freerdpgui package/opt/freerdpgui
+cp resource/freerdpgui-icon.svg package/usr/share/icons/hicolor/scalable/apps/freerdpgui-icon.svg
+cp resource/freerdpgui-mime.xml package/usr/share/mime/packages/freerdpgui-mime.xml
+cp resource/FreerdpGUI.desktop package/usr/share/applications
+
+# Change permissions
+find package/opt/freerdpgui -type f -exec chmod 644 -- {} +
+find package/opt/freerdpgui -type d -exec chmod 755 -- {} +
+find package/usr/share -type f -exec chmod 644 -- {} +
+chmod +x package/opt/freerdpgui/freerdpgui
